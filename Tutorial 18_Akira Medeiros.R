b6aa658f1c00c3b805ac561fc6daad56e44b068d
akira.pinto@gmail.com <fcslab18@FFLCH.LOCAL>
2019-05-31 17:58:08 -0300

    Todas as alterações do tutorial 18 até: Apresentações

---
+++ Tutorial 18_Akira Medeiros.R
@@ -0,0 +1,7 @@
+## Tutorial 18
+## 31 de maio de 2019
+
+## Depois de aprender como usar Rmarkdown, o próximo passo para cientistas de dados com interesses acadêmicos é aprender o Latex para ganhar mais controle sobre os nossos documentos.
+## Latex é uma linguagem de preparação de documentos que podemos acessar direitamente usando, por exemplo, o aplicativo ‘tekmaker’ ou ‘texworks’. Assim, podemos produzir documentos ‘státicos’ sem código ou análise de R e apenas escrevendo a gramática de Latex.  
+## Contudo, o jeito mas sofisticado e flexível para usar Latex é dentro de Rstudio, juntando o conteúdo do seu documento com o análise e outputs do R, exatamento usando o mesmo modelo de Rmarkdown.
+
